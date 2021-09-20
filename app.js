27043db4bf541373f3c13e5b0ecadf181272bae3
Amrinder Singh <amrinder1650@gmail.com>
2021-09-19 23:01:40 -0700

    Completed Bare Minimum

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // TODO
+var App = () => (
+  <div>
+    <GroceryList items = {["Kale", "Cucumbers", "Pizza"]}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item = {item}/>
+    )}
+  </ul>
+);
+
+
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onHoverItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style}
+      onMouseEnter={this.onHoverItemClick.bind(this)}
+      onMouseLeave={this.onHoverItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
